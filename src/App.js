b293a232ece5b0948fa354e80126ad70749ec327
Kevin Sasse <sassek70@gmail.com>
2022-10-19 09:21:00 -0400

    finished code along

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
+
   useEffect(() => {
+    //1st arg: side effect (callback function)
     console.log("useEffect called");
-  });
+
+    //2nd arg: dependencies array
+  }, [count]);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
-}
+
+  useEffect(()=>{
+  document.title = text
+},[text])
+
+useEffect(() => {
+  setTimeout(() => setCount(0), 3000)
+},[count])
+
+
+  const handleClick = () => {
+    setCount((count) => count + 1)
+  }
+
+  const handleChange = (event) => {
+    setText(event.target.value)
+  }
+
+
+  return(
+    <>
+    <div> 
+      <button onClick={handleClick}>I've been clicked {count} times</button>;
+      <input type="text" placeholder="Type away..." value={text} onChange={handleChange}/>
+    </div>
+    <DogPics />
+    </>
+)}
 
 export default App;
